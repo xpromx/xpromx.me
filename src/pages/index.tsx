a726386abb3ddcc8939e351da1d63ec5012627b7
Rodrigo Ramirez <xpromx@gmail.com>
2020-09-13 00:01:48 +0900

    feat: refactor pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,20 @@
-import Head from 'next/head'
-import { Layout } from '@components';
+import React from "react";
+import Head from "next/head";
+import { Layout } from "@components";
 
-export default function Home() {
+const Page = () => {
   return (
     <Layout title="Home">
       <Layout.Main>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+        <Head>
+          <title>Create Next App</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <h1>Hello World!</h1>
+        <h1>Hello World!</h1>
       </Layout.Main>
     </Layout>
-  )
-}
+  );
+};
+
+export default Page;
