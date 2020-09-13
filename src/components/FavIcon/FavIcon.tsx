b976c830a6d7982a5657b2d758fa13c7e5dc4b25
Rodrigo Ramirez <xpromx@gmail.com>
2020-09-13 15:44:12 +0900

    feat: FavIcons

---
+++ src/components/FavIcon/FavIcon.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export const FavIcon = () => (
+  <>
+    <link
+      rel="apple-touch-icon"
+      sizes="180x180"
+      href="/icon/apple-touch-icon.png"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="32x32"
+      href="/icon/favicon-32x32.png"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="16x16"
+      href="/icon/favicon-16x16.png"
+    />
+    <link rel="icon" href="/icon/favicon.ico" />
+    <link rel="manifest" href="/icon/manifest.json" />
+  </>
+);
