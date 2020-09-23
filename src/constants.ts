32ec644848684d640e3c0adc7ff1cdee8ba55a0a
Rodrigo Ramirez <xpromx@gmail.com>
2020-09-23 23:47:46 +0900

    feat(page/index): create page

--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,9 @@ export const SITE_NAME = "Rodrigo Ramirez";
 export const SITE_DESCRIPTION =
   "I am a Full-Stack Developer with 10+ years of experience in design, development, and deployment of enterprise-scale websites.";
 export const DEFAULT_LOCALE = "en";
+
+// SOCIAL NETWORKS
+export const GITHUB = "https://github.com/xpromx";
+export const TWITTER = "https://twitter.com/xpromx";
+export const LINKEDIN = "https://www.linkedin.com/in/jrramirez/";
+export const EMAIL = "xpromx@gmail.com";
