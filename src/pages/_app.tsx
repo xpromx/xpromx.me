af0aef718b59c47ce815348edee52fd8221042bf
Rodrigo Ramirez <xpromx@gmail.com>
2020-09-26 17:45:46 +0900

    feat(styles): move files to styles folder

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,11 @@
-import "../styles.css";
 import React from "react";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "@helpers/ThemeContext";
+
+// styles
+import "../styles/main.css";
+import "../styles/prismjs.css";
+import "../styles/notion.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
