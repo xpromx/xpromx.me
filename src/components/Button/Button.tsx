cf77b6508adaddf0fcd0b97cbf41425602c26d12
xpromx <xpromx@gmail.com>
2020-09-26 15:33:45 +0900

    refactor(components): Button styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -7,16 +7,17 @@ interface ButtonProps {
 export const Button: FC<
   ButtonProps & ButtonHTMLAttributes<HTMLButtonElement>
 > = ({ children, className = "", ...props }) => {
-  const defaultStyles =
-    "text-black border-black  hover:bg-black  hover:text-white";
+  const defaultStyles = "text-black border-black";
 
-  const darkModeStyles =
-    "dark:text-white dark:border-white dark:hover:bg-white dark:hover:text-black";
+  const darkModeStyles = "dark:text-white dark:border-white";
+
+  const animationStyles =
+    "transform transition-all duration-100 ease-in-out hover:scale-105";
 
   return (
     <button
       {...props}
-      className={`px-6 py-3 outline-none rounded border  ${defaultStyles} ${darkModeStyles} ${className}`}
+      className={`px-6 py-3 outline-none rounded-md border font-medium text-lg ${animationStyles} ${defaultStyles} ${darkModeStyles} ${className}`}
     >
       {children}
     </button>
