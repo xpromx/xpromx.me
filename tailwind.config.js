35b7a5f62ac5d1f4fce1e97fe1687e4166956fac
Rodrigo Ramirez <xpromx@gmail.com>
2023-05-05 06:03:21 +0900

    feat: tailwindcss.config.js update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 const { colors } = require("tailwindcss/defaultTheme");
 
-const gray = {
-  100: "hsl(0, 0%, 98%)",
-  200: "hsl(0, 0%, 96%)",
-  300: "hsl(0, 0%, 93%)",
-  400: "hsl(0, 0%, 87%)",
-  500: "hsl(0, 0%, 66%)",
-  600: "hsl(0, 0%, 50%)",
-  700: "hsl(0, 0%, 29%)",
-  800: "hsl(0, 0%, 20%)",
-  900: "hsl(0, 0%, 15%)",
-};
-
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     colors: {
       ...colors,
-      gray,
+      gray: {
+        100: "hsl(0, 0%, 98%)",
+        200: "hsl(0, 0%, 96%)",
+        300: "hsl(0, 0%, 93%)",
+        400: "hsl(0, 0%, 87%)",
+        500: "hsl(0, 0%, 66%)",
+        600: "hsl(0, 0%, 50%)",
+        700: "hsl(0, 0%, 29%)",
+        800: "hsl(0, 0%, 20%)",
+        900: "hsl(0, 0%, 15%)",
+      },
     },
-  },
-  variants: {
-    padding: ({ after }) => after(["last", "first"]),
-    margin: ({ after }) => after(["last", "first"]),
   },
 };
